b00c44a86f722c7c051a66d1708ad28cc9d6ace3
Mateusz <matsma2468@gmail.com>
2024-11-21 17:36:36 +0100

    zapisywanie do pliku bin

--- BST.cpp
+++ BST.cpp
@@ -8,10 +8,29 @@ using namespace std;
 
 
 // Konstruktor klasy BST
+/**
+ * @brief Konstruktor domyslny klasy BST.
+ *
+ * Inicjalizuje korzen drzewa jako `nullptr`.
+ */
 BST::BST() : root(nullptr) {}
 
-// Destruktor klasy BST
+/**
+ * @brief Destruktor klasy BST.
+ *
+ * Niszczy drzewo. Upewnia sie, ze wszystkie wezly zostaly poprawnie usuniete.
+ */
 BST::~BST() {}
+
+/**
+ * @brief Dodaje klucz do drzewa BST.
+ *
+ * Tworzy nowy wezel i wstawia go na odpowiednia pozycje w drzewie.
+ *
+ * @param current Wskaznik na aktualny wezel drzewa.
+ * @param key Wartosc klucza do dodania.
+ * @return Wskaznik na aktualny wezel po dodaniu nowego elementu.
+ */
 
 
 // dodaj do drzewa
@@ -33,6 +52,20 @@ BST::Node* BST::insert(Node* current, int key) {
 
     return current; // Zwracamy bie¿¹cy wêze³
 }
+/**
+ * @brief Usuwa wezel z drzewa BST.
+ *
+ * Funkcja rekursywnie przeszukuje drzewo w celu znalezienia wezla o podanym kluczu
+ * i usuwa go. Usuwanie moze odbywac sie na trzy sposoby:
+ * - Wezel nie ma dzieci (jest liœciem) - usuwa wezel.
+ * - Wezel ma jedno dziecko (lewe lub prawe) - usuwa wezel i przypisuje odpowiednie dziecko.
+ * - Wezel ma dwoje dzieci - znajduje najmniejszy wezel w prawym poddrzewie, przepisuje jego
+ *   klucz do usuwanego wezla i usuwa ten najmniejszy wezel.
+ *
+ * @param current Wskaznik na aktualny wezel drzewa, od ktorego rozpoczyna sie przeszukiwanie.
+ * @param key Klucz wezla, ktory ma zostac usuniety.
+ * @return Wskaznik na aktualny wezel po usunieciu.
+ */
 //usun element z drzewa
 BST::Node* BST::remove(Node* current, int key) {
     if (current == nullptr) {
@@ -74,7 +107,22 @@ BST::Node* BST::remove(Node* current, int key) {
     return current;
 }
 
-
+/**
+ * @brief Zapisuje drzewo do pliku w zadanej kolejnosci.
+ *
+ * Funkcja rekurencyjnie przeszukuje drzewo i zapisuje klucze wezlow do pliku w zaleznosci od typu
+ * przejscia drzewa:
+ * - Inorder: left, root, right
+ * - Preorder: root, left, right
+ * - Postorder: left, right, root
+ *
+ * @param node Wskaznik na obecny wezel.
+ * @param file Strumien pliku, do ktorego zapisywane sa klucze wezlow.
+ * @param type Typ przejscia drzewa:
+ *              1 - inorder
+ *              2 - preorder
+ *              3 - postorder
+ */
 void BST::saveToFile(Node* node, std::ofstream& file, int type) {
     if (node == nullptr) {
         return;
@@ -110,12 +158,24 @@ void BST::saveToFileP(const std::string& filename, int type) {
     std::ofstream file(filename);
     saveToFile(root, file, type); // Wywo³anie metody rekurencyjnej
     file.close();
+    std::ofstream fileBin("treeBin.bin", ios::binary);
+    saveToFile(root, file, type); // Wywo³anie metody rekurencyjnej
+    fileBin.close();
 }
 
 
 
 //Funkcje ukladajace drzewo
 // inorder
+/**
+ * @brief Przechodzi drzewo BST w porzadku inorder (lewy, korzen, prawy).
+ *
+ * Funkcja rekurencyjnie przechodzi drzewo BST w porzadku inorder, czyli najpierw przeszukuje
+ * lewe poddrzewo, nastepnie wypisuje klucz wezla (korzen), a na koniec przeszukuje prawe poddrzewo.
+ * Porzadek ten jest wykorzystywany m.in. do uzyskania posortowanej listy kluczy w drzewie.
+ *
+ * @param root Wskaznik na aktualny wezel, od ktorego rozpoczyna sie przechodzenie drzewa.
+ */
 void BST::inorder(Node* root)
 {
     if (root != nullptr) {
@@ -124,7 +184,14 @@ void BST::inorder(Node* root)
         inorder(root->right);
     }
 }
-
+/**
+ * @brief Przechodzi drzewo BST w porzadku preorder (korzen, lewy, prawy).
+ *
+ * Funkcja rekurencyjnie przechodzi drzewo BST w porzadku preorder, czyli najpierw wypisuje
+ * klucz wezla (korzen), nastepnie przeszukuje lewe poddrzewo, a na koniec prawe poddrzewo.
+ *
+ * @param current Wskaznik na aktualny wezel, od ktorego rozpoczyna sie przechodzenie drzewa.
+ */
 void BST::preorder(Node* current)
 {
     if (current != nullptr) {
@@ -133,7 +200,14 @@ void BST::preorder(Node* current)
         preorder(current->right);
     }
 }
-
+/**
+ * @brief Przechodzi drzewo BST w porzadku postorder (lewy, prawy, korzen).
+ *
+ * Funkcja rekurencyjnie przechodzi drzewo BST w porzadku postorder, czyli najpierw przeszukuje
+ * lewe poddrzewo, potem prawe poddrzewo, a na koniec wypisuje klucz wezla (korzen).
+ *
+ * @param current Wskaznik na aktualny wezel, od ktorego rozpoczyna sie przechodzenie drzewa.
+ */
 void BST::postorder(Node* current)
 {
     if (current != nullptr) {
@@ -159,7 +233,11 @@ BST::Node* BST::findMin(Node* current) {
     }
     return current;
 }
-
+/**
+ * @brief Usuwa cale drzewo BST.
+ *
+ * Funkcja usuwa cale drzewo BST, usuwajac wszystkie wezly i resetujac korzen drzewa.
+ */
 void BST::removeTree(Node* current)
 {
     if (current != nullptr) {
@@ -168,24 +246,48 @@ void BST::removeTree(Node* current)
         delete current;        // Usuñ bie¿¹cy wêze³
     }
 }
-
+/**
+ * @brief Wyswietla drzewo w porzadku inorder.
+ *
+ * Funkcja wypisuje wszystkie klucze w drzewie w porzadku inorder (lewy, korzen, prawy).
+ * Jest to jeden ze sposobow przechodzenia drzewa, ktory daje posortowane elementy.
+ */
 //----------Funkcje publiczne ---------
 void BST::show_inorder() {
     cout << "Inorder: "; 
     inorder(root);
     cout << endl;
 }
+/**
+ * @brief Wyswietla drzewo w porzadku preorder.
+ *
+ * Funkcja wypisuje wszystkie klucze w drzewie w porzadku preorder (korzen, lewy, prawy).
+ * Jest to inny sposob przechodzenia drzewa, w ktorym najpierw przetwarzamy korzen.
+ */
 void BST::show_preorder(){
     cout << "Preorder: ";
     preorder(root);
     cout << endl; // Dodanie nowej linii po wyœwietleniu
 }
-
+/**
+ * @brief Wyswietla drzewo w porzadku postorder.
+ *
+ * Funkcja wypisuje wszystkie klucze w drzewie w porzadku postorder (lewy, prawy, korzen).
+ * Jest to sposób przechodzenia drzewa, w którym najpierw przetwarzamy dzieci przed rodzicem.
+ */
 void BST::show_postorder(){
     cout << "Postorder: ";
     postorder(root);
     cout << endl; // Dodanie nowej linii po wyœwietleniu
 }
+/**
+ * @brief Usuwa wezel z drzewa BST.
+ *
+ * Funkcja usuwa wezel o zadanym kluczu z drzewa. W przypadku znalezienia wezla,
+ * zostaje on usuniety, a drzewo zostaje odpowiednio zaktualizowane.
+ *
+ * @param key Klucz wezla, ktory ma zostac usuniety.
+ */
 void BST::remove(int key) {
     root = remove(root, key);
 }
--- BST.h
+++ BST.h
@@ -1,57 +1,174 @@
-#pragma once
-#ifndef BST_H
-#define BST_H
-#include <fstream>
-#include <string>
+/**
+ * @brief Klasa reprezentuj¹ca drzewo BST (Binary Search Tree).
+ *
+ * Klasa implementuje drzewo binarne wyszukiwania, umo¿liwiaj¹c dodawanie, usuwanie,
+ * przeszukiwanie i zapisywanie drzewa do pliku. Operacje te s¹ wykonywane zgodnie z zasadami
+ * drzewa BST, gdzie ka¿dy wêze³ ma maksymalnie dwóch potomków: lewego i prawego.
+ */
 class BST {
 private:
+    /**
+     * @brief Struktura reprezentuj¹ca wêze³ drzewa BST.
+     *
+     * Struktura zawiera wskaŸniki na rodzica oraz potomków (lewego i prawego),
+     * a tak¿e klucz (wartoœæ) przechowywan¹ w wêŸle.
+     */
     struct Node {
-        Node* p;        // WskaŸnik na rodzica
-        Node* left;     // WskaŸnik na lewego potomka
-        Node* right;    // WskaŸnik na prawego potomka
-        int key;        // Klucz (wartoœæ wêz³a)
+        Node* p;        ///< WskaŸnik na rodzica
+        Node* left;     ///< WskaŸnik na lewego potomka
+        Node* right;    ///< WskaŸnik na prawego potomka
+        int key;        ///< Klucz (wartoœæ) przechowywana w wêŸle
 
-        // Konstruktor dla u³atwienia inicjalizacji wêz³ów
+        /**
+         * @brief Konstruktor inicjalizuj¹cy wêze³ z kluczem.
+         *
+         * Ustawia wskaŸniki na rodzica i potomków na nullptr oraz przypisuje
+         * wartoœæ klucza.
+         *
+         * @param k Klucz (wartoœæ) wêz³a.
+         */
         Node(int k) : p(nullptr), left(nullptr), right(nullptr), key(k) {}
     };
-    Node* root;     //wskaznik na korzen drzewa
 
-    Node* insert(Node* current, int key);       //dodaj do drzewa
-    Node* remove(Node* current, int key); // Rekurencyjna funkcja usuwania
+    Node* root;  ///< Korzeñ drzewa
+
+    /**
+     * @brief Rekurencyjna funkcja dodaj¹ca nowy klucz do drzewa.
+     *
+     * Funkcja wstawia nowy klucz do drzewa BST, zachowuj¹c jego strukturaln¹ w³aœciwoœæ.
+     * @param current WskaŸnik na bie¿¹cy wêze³.
+     * @param key Klucz, który ma zostaæ dodany.
+     * @return WskaŸnik na nowy korzeñ (lub pozosta³y korzeñ) poddrzewa.
+     */
+    Node* insert(Node* current, int key);
+
+    /**
+     * @brief Rekurencyjna funkcja usuwaj¹ca wêze³ o danym kluczu.
+     *
+     * Funkcja usuwa wêze³ o zadanym kluczu z drzewa, zachowuj¹c strukturaln¹ w³aœciwoœæ drzewa BST.
+     * @param current WskaŸnik na bie¿¹cy wêze³.
+     * @param key Klucz wêz³a, który ma zostaæ usuniêty.
+     * @return WskaŸnik na nowy korzeñ (lub pozosta³y korzeñ) poddrzewa.
+     */
+    Node* remove(Node* current, int key);
+
+    /**
+     * @brief Funkcja znajduj¹ca najmniejszy wêze³ w drzewie.
+     *
+     * Funkcja przeszukuje lewe poddrzewo, a¿ znajdzie wêze³ o najmniejszym kluczu.
+     * @param current WskaŸnik na bie¿¹cy wêze³.
+     * @return WskaŸnik na najmniejszy wêze³.
+     */
     Node* findMin(Node* current);
-    void removeTree(Node* current); // usun cale drzewo
+
+    /**
+     * @brief Funkcja usuwaj¹ca ca³e drzewo BST.
+     *
+     * Funkcja rekurencyjnie usuwa wszystkie wêz³y w drzewie.
+     * @param current WskaŸnik na bie¿¹cy wêze³.
+     */
+    void removeTree(Node* current);
+
+    /**
+     * @brief Funkcja zapisuj¹ca drzewo do pliku w okreœlonym porz¹dku.
+     *
+     * Funkcja zapisuje klucze drzewa do pliku w porz¹dku zale¿nym od zadanego typu.
+     * @param node WskaŸnik na wêze³, który ma zostaæ zapisany.
+     * @param file Referencja na obiekt pliku, do którego bêd¹ zapisywane dane.
+     * @param type Typ przejœcia drzewa (1 - inorder, 2 - preorder, 3 - postorder).
+     */
     void saveToFile(Node* node, std::ofstream& file, int type);
 
-
-    
-    //ukladanie drzewa
+    // Funkcje przeszukiwania drzewa
+    /**
+     * @brief Funkcja przechodz¹ca drzewo w porz¹dku inorder.
+     *
+     * Funkcja rekurencyjnie przechodzi drzewo i wyœwietla klucze w porz¹dku inorder (lewy, korzeñ, prawy).
+     * @param root WskaŸnik na korzeñ poddrzewa.
+     */
     void inorder(Node* root);
+
+    /**
+     * @brief Funkcja przechodz¹ca drzewo w porz¹dku preorder.
+     *
+     * Funkcja rekurencyjnie przechodzi drzewo i wyœwietla klucze w porz¹dku preorder (korzeñ, lewy, prawy).
+     * @param current WskaŸnik na bie¿¹cy wêze³.
+     */
     void preorder(Node* current);
+
+    /**
+     * @brief Funkcja przechodz¹ca drzewo w porz¹dku postorder.
+     *
+     * Funkcja rekurencyjnie przechodzi drzewo i wyœwietla klucze w porz¹dku postorder (lewy, prawy, korzeñ).
+     * @param current WskaŸnik na bie¿¹cy wêze³.
+     */
     void postorder(Node* current);
-    
+
 public:
-    // Konstruktor i destruktor
+    /**
+     * @brief Konstruktor klasy BST.
+     *
+     * Inicjalizuje puste drzewo (korzeñ ustawiony na nullptr).
+     */
     BST();
+
+    /**
+     * @brief Destruktor klasy BST.
+     *
+     * Usuwa wszystkie wêz³y w drzewie, aby zwolniæ pamiêæ.
+     */
     ~BST();
-    // Funkcja dodaj¹ca element do drzewa
+
+    /**
+     * @brief Funkcja dodaj¹ca nowy klucz do drzewa.
+     *
+     * Funkcja wywo³uje rekurencyjn¹ wersjê `insert` w celu dodania nowego klucza.
+     * @param key Klucz, który ma zostaæ dodany do drzewa.
+     */
     void insert(int key);
 
-    //Funkcja usuwaj¹ca element
+    /**
+     * @brief Funkcja usuwaj¹ca wêze³ o danym kluczu.
+     *
+     * Funkcja wywo³uje rekurencyjn¹ wersjê `remove` w celu usuniêcia wêz³a.
+     * @param key Klucz wêz³a, który ma zostaæ usuniêty.
+     */
     void remove(int key);
+
+    /**
+     * @brief Funkcja usuwaj¹ca ca³e drzewo.
+     *
+     * Funkcja wywo³uje rekurencyjn¹ wersjê `removeTree` w celu usuniêcia ca³ego drzewa.
+     */
     void removeTree();
-    //Funkcja szukajaca drogi do podaniego elementu
 
-
-    //Zapisz do pliku txt drzewo
+    /**
+     * @brief Funkcja zapisuj¹ca drzewo do pliku w okreœlonym porz¹dku.
+     *
+     * Funkcja zapisuje drzewo do pliku w formacie tekstowym w zadanym porz¹dku.
+     * @param filename Nazwa pliku, do którego drzewo zostanie zapisane.
+     * @param type Typ przejœcia drzewa (1 - inorder, 2 - preorder, 3 - postorder).
+     */
     void saveToFileP(const std::string& filename, int type);
 
-
-    // Funkcja wyœwietlaj¹ca drzewo w porz¹dku inorder
+    /**
+     * @brief Funkcja wyœwietlaj¹ca drzewo w porz¹dku inorder.
+     *
+     * Funkcja wywo³uje rekurencyjn¹ funkcjê `inorder` i wypisuje elementy drzewa w porz¹dku inorder.
+     */
     void show_inorder();
+
+    /**
+     * @brief Funkcja wyœwietlaj¹ca drzewo w porz¹dku preorder.
+     *
+     * Funkcja wywo³uje rekurencyjn¹ funkcjê `preorder` i wypisuje elementy drzewa w porz¹dku preorder.
+     */
     void show_preorder();
+
+    /**
+     * @brief Funkcja wyœwietlaj¹ca drzewo w porz¹dku postorder.
+     *
+     * Funkcja wywo³uje rekurencyjn¹ funkcjê `postorder` i wypisuje elementy drzewa w porz¹dku postorder.
+     */
     void show_postorder();
-
-
 };
-
-#endif // BST_H
